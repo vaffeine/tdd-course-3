173cf9c82032cba31c5818c24d855af366395e81
Vlad Alex <vlad.al.dp@gmail.com>
2018-10-31 20:47:34 +0200

    implemented check for divisible by 100

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -16,7 +16,7 @@ If your language provides a method in the standard library that does this look-u
 
 bool is_leap_year(uint64_t year)
 {
-    return year % 400 == 0 || year % 4 == 0;
+    return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
 }
 
 TEST(LeapYear, TestDivisible400)
