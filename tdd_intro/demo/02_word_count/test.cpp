3f7bd22eebfa788615ab57a0f720cecdeaf2b06d
Vlad Alex <vlad.al.dp@gmail.com>
2018-11-07 21:14:50 +0200

    added test for multiple words

--- tdd_intro/demo/02_word_count/test.cpp
+++ tdd_intro/demo/02_word_count/test.cpp
@@ -41,3 +41,20 @@ TEST(count_words, test_one_word_ret_one_word) {
     ASSERT_EQ(count_words(word), expected);
 }
 
+TEST(count_words, test_multiple_words_ret_correct) {
+    std::string phrase = "olly olly in come free please please let it be in such manner olly";
+    std::map<std::string, uint32_t> expected = {
+        {"olly", 3},
+        {"in", 2},
+        {"come", 1},
+        {"free", 1},
+        {"please", 2},
+        {"let", 1},
+        {"it", 1},
+        {"be", 1},
+        {"manner", 1},
+        {"such", 1},
+    };
+    ASSERT_EQ(count_words(phrase), expected);
+}
+
